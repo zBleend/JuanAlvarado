092b180615db2425869012d3f576ea8ac0ba7a80
zBleend <juancrichile@gmail.com>
2025-04-30 12:49:04 -0400

    commit 6

--- Python/BANCO EN LINEA.py
+++ Python/BANCO EN LINEA.py
@@ -23,7 +23,7 @@ def menu():
     time.sleep(2)
     limpiar_pantalla()
     print("""
-Banco en linea
+------Banco en línea------
 
 1.- Iniciar sesion
 2.- Registrarse
@@ -36,22 +36,21 @@ def validacion(mensaje, mensaje_error, rango):
     try:
         while True:
             var=input(mensaje)
-            if var.isdigit and int(var) in rango:
+            if var.isdigit() and int(var) in rango:
                 var=int(var)
                 return (var)
             else:
                 print(mensaje_error)
-    except ValueError or NameError:
+    except (ValueError, NameError):
         print(mensaje_error)
 
-def sistema(op, usuario_registrado, saldos_usuarios):
+def sistema(op, usuario_registrado, saldos_usuarios, billetes):
     if op==1:
-        # inicio_sesion(usuario_registrado)
         user = inicio_sesion(usuario_registrado)
         if user:
             while True:
                 op2 = menu_cajero(user)
-                if sistema_cajero(op2, user, saldos_usuarios) == "salir":
+                if sistema_cajero(op2, user, saldos_usuarios, billetes) == "salir":
                     break
     elif op==2:
         registro(usuario_registrado, saldos_usuarios)
@@ -86,34 +85,137 @@ def menu_cajero(user):
     print("""
 1.- Ver saldo
 2.- Transferir
-3.- Salir
+3.- Retirar dinero
+4.- Salir
 """)
-    return validacion("Seleccione una opcion: ", "Seleccione una opcion valida [1,3]", range(1,4))
+    return validacion("Seleccione una opcion: ", "Seleccione una opcion valida [1,4]", range(1,5))
 
-def sistema_cajero(op2, user, saldos_usuarios):
+def sistema_cajero(op2, user, usuario_registrado, saldos_usuarios, billetes):
     if op2==1:
         print(f"Su saldo actual es de: ${saldos_usuarios[user]}")
-        input("\nPrecione Enter para continuar...")
+        input("\nPresione Enter para continuar...")
         return "continuar"
     elif op2==2:
-        print("Aun en desarrollo")
-        time.sleep(1)
+        transferir(user, saldos_usuarios, usuario_registrado)
         return "continuar"
     elif op2==3:
+        retiro(user, saldos_usuarios, billetes)
+        return "continuar"
+    elif op2==4:
         print("Cerrando sesion...")
-        time.sleep(1)
+        time.sleep(1.5)
         limpiar_pantalla()
-        return "Salir"
+        return "salir"
+
+def transferir(user, saldos_usuarios, usuario_registrado):
+    limpiar_pantalla()
+    destinatario=input(f"""
+------Trasferir------   
+Saldo actual: {saldos_usuarios[user]}
+Ingrese el nombre del destinatario: 
+""")
+
+    if destinatario not in usuario_registrado:
+        print("\nError: El destinatario no existe")
+        time.sleep(1)
+        return
+
+    if destinatario == user:
+        print("Error: No puedes trasferirte a ti mismo")
+        time.sleep(1)
+        return
+
+    while True:
+        try:
+            monto=int(input("\nIngresa el monto a trasferir:            (Minimo $500)\n$"))
+            if monto< 500:
+                print("El monto tiene que ser como minimo arriba de $500")
+                continue
+            if monto>saldos_usuarios[user]:
+                print("Fondos insuficientes")
+                continue
+            print(f"\n¿Desea transferir ${monto} a {destinatario}?")
+            conf=input("""
+1.- Si
+2.- No""")
+            if conf=="1":
+                saldos_usuarios[user] -= monto
+                saldos_usuarios[destinatario] += monto
+                input(f"""
+Trasferencia exitosa
+Nuevo saldo: ${saldos_usuarios[user]}
+Preciones Enter para continuar...
+""")
+                return
+            elif conf=="2":
+                print("\nTrasferencia cancelada")
+                time.sleep(1)
+                return
+        except ValueError:
+            print("Ingrese un monto valido")
+
+def retiro(user, saldo_usuarios, billetes):
+    limpiar_pantalla()
+    print(f"""
+------Retiro de efectivo------
+Tu saldo actual es de: ${saldo_usuarios[user]}
+Billetes disponibles: $5.000 | $10.000 | $15.000 | $20.000 |
+""")
+    while True:
+        try:
+            monto=int(input("\nIngresa el monto a retirar:            (Minimo $5.000)\n$"))
+            if monto <= 5000:
+                print("Error: El minimo a retirar es $5.000")
+                continue
+            if monto > saldo_usuarios[user]:
+                print("Error: Fondos insuficientes")
+                continue
+            if not es_monto_valido(monto, billetes):
+                print("Error: El monto debe ser múltiplo de $5.000")
+                continue
+            if realizar_retiro(monto, billetes):
+                saldo_usuarios[user] -= monto
+                input(f"""
+Retiro exitoso
+Nuevo saldo: ${saldo_usuarios[user]}
+Presione Enter para continuar...
+""")
+                return
+            else:
+                print("No hay suficientes billetes para este monto")
+        except ValueError:
+            print("Ingrese un monto valido")
+
+def es_monto_valido(monto, billetes):
+    return monto % 5000 == 0
+
+def realizar_retiro(monto, billetes):
+    temp_billetes=billetes.copy()
+    denominaciones = sorted(temp_billetes.keys(), reverse=True)
+    for denom in denominaciones:
+        if monto >= denom:
+            cant= min(monto//denom, temp_billetes[denom])
+            monto -= denom*cant
+            temp_billetes[denom] -= cant
+    if monto == 0: 
+        for denom in billetes:
+            billetes[denom] = temp_billetes[denom]
+        return True
+    return False
 
 def registro(usuario_registrado, saldos_usuarios):
     print("------Registro------")
-    user=input("Ingrese su nombre de usuario para registrarse: \n")
-    if user in usuario_registrado:
-        print("Error, este nombre ya fue registrado") 
-        return None
+    while True:
+        user=input("Ingrese su nombre de usuario para registrarse: \n").strip()
+        if user in usuario_registrado:
+            print("Error, este nombre ya fue registrado") 
+            continue
+        elif not user.strip():
+            print("Error: El nombre no puede estar vacío")
+        else:
+            break
     clave_user=clave()
-    saldo_inicial = random.choice([5000, 10000, 20000])
-
+    saldo_inicial = random.choice([5000, 10000, 15000, 20000])
     usuario_registrado[user] = clave_user
     saldos_usuarios[user] = saldo_inicial
 
@@ -137,11 +239,21 @@ def clave():
             print(f"Error {mensaje}. Intente nuevamente")
 
 def registos():
-    usuario_registrado={}
-    saldos_usuarios={}
-    return usuario_registrado, saldos_usuarios
+    usuario_registrado ={}
+    saldos_usuarios ={}
+    billetes ={
+        20000:30,
+        15000:30,
+        10000:30,
+        5000:30
+    }
+    return usuario_registrado, saldos_usuarios, billetes
 
 if __name__=="__main__":
-    usuario_registrado, saldos_usuarios = registos()
-    op=menu()
-    op=sistema(op, usuario_registrado, saldos_usuarios)
+    usuario_registrado, saldos_usuarios, billetes = registos()
+    while True:
+
+        op=menu()
+        sistema(op, usuario_registrado, saldos_usuarios, billetes)
+        if op != 3:
+            limpiar_pantalla()
--- Python/Suma de numeros.py
+++ Python/Suma de numeros.py
@@ -1,6 +1,8 @@
-def calculadora():
-    n1=int(input("Ingresa un numero: "))
-    n2=int(input("Ingresa otro numero: "))
-    suma=n1+n2
-    print("La suma de tus numeros es:", suma)
-calculadora()
+# def calculadora():
+#     n1=int(input("Ingresa un numero: "))
+#     n2=int(input("Ingresa otro numero: "))
+#     suma=n1+n2
+#     print("La suma de tus numeros es:", suma)
+# calculadora()
+
+monto=int(input("\nIngresa el monto a trasferir:            (Minimo $500)\n$"))
